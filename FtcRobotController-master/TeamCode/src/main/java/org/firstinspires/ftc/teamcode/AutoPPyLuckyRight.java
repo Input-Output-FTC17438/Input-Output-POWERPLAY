e520b21a2570b38663f1192a17dbccbf439cf3e9
Input-Output-FTC17438 <114227724+Input-Output-FTC17438@users.noreply.github.com>
2022-11-18 15:50:33 +0300

    Temp changes to set servos

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPPyLuckyRight.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPPyLuckyRight.java
@@ -10,19 +10,22 @@ public class AutoPPyLuckyRight extends LinearOpMode {
     @Override
     public void runOpMode(){
         hw.init();
-        hw.activateEncodersMove();
-        hw.activateEncodersLift();
-        hw.deActivateEncodersIntake();
-        waitForStart();
-        if (opModeIsActive()){
-            hw.moveEnc(0 ,1, 0, 1000, true);
-            hw.liftEnc(1000);
-            hw.moveEnc(0, 0.5, 0, 100, false);
-            hw.intakeRunTimer(1, 1000);
-            hw.liftEnc(0);
-            hw.moveEnc(0, -1, 0, 500, true);
-            hw.moveEnc(0, 0, -0.5, 100, false);
-            hw.moveEnc(0, 1, 0, 1000, true);
+        if (opModeIsActive()) {
+            hw.servoRun(0);
         }
+//        hw.activateEncodersMove();
+//        hw.activateEncodersLift();
+//        hw.deActivateEncodersIntake();
+//        waitForStart();
+//        if (opModeIsActive()){
+//            hw.moveEnc(0 ,1, 0, 1000, true);
+//            hw.liftEnc(1000);
+//            hw.moveEnc(0, 0.5, 0, 100, false);
+//            hw.intakeRunTimer(1, 1000);
+//            hw.liftEnc(0);
+//            hw.moveEnc(0, -1, 0, 500, true);
+//            hw.moveEnc(0, 0, -0.5, 100, false);
+//            hw.moveEnc(0, 1, 0, 1000, true);
+//        }
     }
 }
